23451de8ba5552f29098f4d55aad0f8dc0e0d997
Brian Shockency <53105161+Bshock817@users.noreply.github.com>
2020-03-19 19:21:34 -0400

    more algos

--- algos/arrays.js
+++ algos/arrays.js
@@ -119,3 +119,71 @@ return arr;
 myArr = [13,57,85,3,2,21,150];
 
 console.log(minToFront(myArr));
+
+//Reverse
+
+function reverse(arr){
+    var temp = arr[0];
+    for(var i = 0; i < arr.length / 2; i++){
+        temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+myArr = [1,2,3,4,5,6];
+console.log(reverse(myArr));
+
+//Rotate
+
+function rotate(arr, offset){
+    var temp = arr[0];
+
+    for(var j = 0; j < offset; j++){
+        for(var i = 0; i < arr.length - 1; i++){
+            temp = arr[i+1];
+            arr[i+1] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    return arr;
+}
+var myArr = [5,11,400,2,3,3,1,32,76,54,6,]
+console.log(rotate(myArr,4));
+
+//Filter Range
+
+function filterRange(arr, min, max){
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] < min || arr[i] > max){
+            var temp = arr[i];
+            for(var j = i; j < arr.length - 1; j++){
+                temp = arr[j+1];
+                arr[j + 1] = arr[j]
+                arr[j] = temp;
+            }
+            arr.pop();
+            i--;
+        }
+        
+    }
+    return arr;
+}
+var myArr2 = [5,11,400,2,3,3,1,32,76,54,6,]
+console.log(filterRange(myArr2, 3,1000));
+
+//Concat
+
+function concat(arr1,arr2){
+    var newArr = [];
+
+    for(var i = 0; i < arr1.length; i++){
+        newArr.push(arr1[i]);
+    }
+    for(var i = 0; i < arr2.length; i++){
+        newArr.push(arr2[i]);
+    }
+    return newArr;
+}
+console.log(concat(myArr,myArr2));
